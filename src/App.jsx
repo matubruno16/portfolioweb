ac95a09e86d8f18ba6810a21407e725930cce1ba
matubruno16 <matubruno16@gmail.com>
2024-02-04 11:32:36 -0300

    correccion importacion

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { NavBar } from "./components/NavBar";
 import { Proyects } from "./components/Proyects";
 import { Services } from "./components/Services";
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource-variable/oswald';
